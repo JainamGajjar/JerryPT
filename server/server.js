1b669ca8868ec16d952159fe05d9b86182d8da4f
JainamGajjar69 <thefenixa30@gmail.com>
2023-04-06 15:04:18 +0530

    changes in logic

--- server/server.js
+++ server/server.js
@@ -28,8 +28,8 @@ app.post('/',async (req,res) => {
         model: "text-davinci-003",
   prompt: `${prompt}`,
   temperature: 0,
-  max_tokens: 3000,
-  top_p: 1,
+  max_tokens: 35000,
+  top_p: 0.5,
   frequency_penalty: 0.5,
   presence_penalty: 0,
   diversity_penalty: 0.2
